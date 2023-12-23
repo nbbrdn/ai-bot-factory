1654b471b3e90f06f78f421b5b3b4e2f8a87c561
Nikolay Bayborodin <nbbrdn@gmail.com>
2023-12-23 20:49:37 +1000

    Fix flake8 issues

--- handlers.py
+++ handlers.py
@@ -4,7 +4,12 @@ import re
 from aiogram import F, Router, flags
 from aiogram.enums import ChatAction
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
 from aiogram.utils.chat_action import ChatActionMiddleware
 
 import external
@@ -19,7 +24,20 @@ threads = {}
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Тут будут две кнопки")
+    kb = [
+        [KeyboardButton(text="Давай разберем случайный кейс")],
+        [KeyboardButton(text="Предложи список из 10 случайных кейсов")],
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Укажите способ выбора кейса",
+    )
+
+    await message.answer(
+        ("Вы хотите выбрать кейс из предложенного списка, или выбрать случайный?"),
+        reply_markup=keyboard,
+    )
 
 
 @router.message(F.text)
@@ -34,10 +52,15 @@ async def message_with_text(message: Message):
 
     prompt = message.text
     logging.info(f"user (id={user_id}): {prompt}")
+    message = await message.answer(
+        "✍️ минутку, пишу ответ ...", reply_markup=ReplyKeyboardRemove()
+    )
 
     text = await external.generate_text(prompt, thread_id)
     text = re.sub(r"\*\*(.+?)\*\*", r"<b>\1</b>", text)
     text = re.sub(r"\*(.+?)\*", r"<i>\1</i>", text)
+
+    await message.delete()
 
     await message.answer(text)
     logging.info(f"bot (id={user_id}): {text}")
